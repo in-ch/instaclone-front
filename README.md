80bd7b2ffb9ed5500f0ec8a76eb2b18e1eb9212c
inch <sxin2949@naver.com>
2021-06-04 20:45:20 +0900

    #8.4 Router Setup part Two

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #210604 프론트 부분 시작 !
+
+#8.3 <Route>만 쓰면 태그가 겹치니깐 <switch>로 묶어 써야함.
+    그리고 <Route>태그에 "exact"옵션을 넣어줘야함 => Hashrouter 
+    BrowserRouter를 쓰려면 deploy할 때 몇 가지 고려 사항을 더 고려해야함.
--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import { BrowserRouter as Router,Switch, Route, Redirect } from "react-router-dom";
+import Home from './screens/Home';
+import Login from './screens/Login';
+import NotFound from './screens/404';
 function App() {
+  const isLoggedIn = false;
+
   return (
     <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home />: <Login />}
+          </Route>
+          <Route path="/inch" exact>
+            {!isLoggedIn ? "로그인해주세요." : "로그인 되어 있네요."}
+          </Route>
+          <Route>
+            <NotFound />
+            {/* <Redirect to="/" />  리다이렉트 시키는 것임 */}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
