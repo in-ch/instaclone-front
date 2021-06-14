b8fa366875c7641e3fc3d6614d9791f4e83b38b7
inch <sxin2949@naver.com>
2021-06-14 22:38:50 +0900

    #11.4 Photo Component part One

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,58 @@
+import { gql, useQuery } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 import { isLoggedInVar, logUserOut } from "../apollo";
+import { FatText } from "../components/shared";
+import styled from "styled-components";
+import Avatar from "../components/Avatar";
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 10px;
+`;
+
+const FEED_QUERY = gql`
+    query seeFeed {
+        seeFeed{
+            id
+            user {
+                userName
+                avatar
+            }
+            file
+            caption
+            likes
+            comments
+            createAt
+            isMine
+        }
+    }
+`;
 
 const Home = () => {
     const history = useHistory();
+    const {data} = useQuery(FEED_QUERY);
+    console.log(data);
 
     return (
         <>
-            <h1>홈</h1>
-            <button onClick={()=>{logUserOut(history)}}>로그아웃 하기</button>
+            {data?.seeFeed?.map((photo) => (
+                <PhotoContainer key={photo.id}>
+                <PhotoHeader>
+                    <Avatar url={photo.user.avatar} />
+                    <Username>{photo.user.userName}</Username>
+                </PhotoHeader>
+                </PhotoContainer>
+            ))}
         </>
     )
     
